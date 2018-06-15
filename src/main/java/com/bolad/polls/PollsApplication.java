3a794b284fac4a01331a7843626818cb9de8fd14
stanley <stanley.akyea@gmail.com>
2018-06-15 20:33:23 +0200

    add repository

--- src/main/java/com/bolad/polls/PollsApplication.java
+++ src/main/java/com/bolad/polls/PollsApplication.java
@@ -1,10 +1,29 @@
 package com.bolad.polls;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+/*register JPA 2.1 converters so that all the Java 8 Date/Time 
+fields in the domain models automatically get converted to SQL 
+types when we persist them in the database.*/
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
 public class PollsApplication {
+	
+	//Set default time zone for application to UTC
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
---
+++ src/main/java/com/bolad/polls/model/RoleName.java
@@ -0,0 +1,9 @@
+package com.bolad.polls.model;
+
+public enum RoleName {
+	
+	ROLE_USER, ROLE_ADMIN
+
+}
+
+
